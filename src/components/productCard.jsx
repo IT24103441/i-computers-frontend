0ca577204108bbab3f0ae2b41dd7878c7fa009cd
IT24103441 <it24103441@my.sliit.lk>
2026-04-22 20:54:06 +0530

    add the new css

--- src/components/productCard.jsx
+++ src/components/productCard.jsx
@@ -3,7 +3,7 @@ export default function ProductCard(){
         <div className="border w-56 h-80 p-4 rounded-lg shadow-md">
             <img src="https://via.placeholder.com/150" alt="Product Image" className="w-40 h-40"/>
             <h3>Product Name</h3>
-            <p>RS. 250000</p>
+            <p className="border-amber-200 bg-amber-400">RS. 250000</p>
             <button className="border bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600">Add to Cart</button>
         </div>
     );
